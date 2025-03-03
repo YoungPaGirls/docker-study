7446db508081398f226ab901f19ac5fb431217f0
yeonkkk <tjddus0302@gmail.com>
2025-03-03 20:45:24 +0900

    김성연 2주차 수정

--- week02/joy/main.go
+++ week02/joy/main.go
@@ -1,24 +1,23 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
-// 서버 애플리케이션
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r * http.Request) {
-        log.PrintIn("received request") // 클라이언트 요청 시 received request 메세지 출력
-        fmt.Fprintf(w, "Hello Docker!!") // 요청이 오면 Hello Docker!! 응답
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("received request")
+		fmt.Fprintf(w, "Hello Docker!")
+	})
 
-    })
+	log.Println("start server")
+	server := &http.Server{
+		Addr: ":8080",
+	}
 
-    log.Println("start server")
-    server: = & http.Server {
-        Addr: ":8080" // 8080 포트로 요청 받음
-    }
-    if err: = server.ListenAndServe(); err != nil {
-     log.PrintIn(err)
-    }
+	if err := server.ListenAndServe(); err != nil {
+		log.Println(err)
+	}
 }
